fff84bf3a91f20fbe29088e105b41fbc329de759
Sophie Gu <sophieqgu@gmail.com>
2019-03-26 17:14:44 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,23 @@
+class Doctor 
+  attr_accessor :name 
+  @@all = []
+  def initialize()
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end 
+  
+  def appointments 
+    Appointment.all.select {|a| a.doctor == self} 
+  end 
+  
+  def patients 
+    Appointment.all.map {|a| a.patient}
+  end 
+end 
